5dda21feaa7522f1b999d906a82200ed37112fa7
ThePriestt <49431616+ThePriestt@users.noreply.github.com>
2021-04-06 15:20:39 +0300

    Add project files.

---
+++ OnlineShopAudi/Data/Migrations/20210406121811_AddingParts.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShopAudi.Data.Migrations
+{
+    public partial class AddingParts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Part",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InventoryNumber = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Discription = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Part", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Part");
+        }
+    }
+}
---
+++ OnlineShopAudi/Models/Part.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopAudi.Models
+{
+    public class Part
+    {
+        public int Id { get; set; }
+        public string InventoryNumber { get; set; }
+        public string Name { get; set; }
+        public string Discription { get; set; }
+        public decimal Price { get; set; }
+    }
+}
